7e8166692be8b6959a5effca4ccb1d9e296526d0
Jakub Biesiada <biesiadajakub@icloud.com>
2019-12-25 20:23:15 +0100

    Clean up

--- src/ui/helpers/parseOptionTitle.ts
+++ src/ui/helpers/parseOptionTitle.ts
@@ -1,13 +1,6 @@
 export const parseOptionTitle = (title: string) => {
-  const words = title.split('_');
-
-  // for (let word of words) {
-  //   return `${word[0].toUpperCase()}${word.slice(1)}`;
-  // }
-
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-  }
-
-  return words.join(' ');
+  return title
+    .split('_')
+    .map(word => `${word[0].toUpperCase()}${word.slice(1)}`)
+    .join(' ');
 };
