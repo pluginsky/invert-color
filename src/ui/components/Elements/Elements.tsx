af32f20a88f209bdad930df30e8d807350e1c9ee
Jakub Biesiada <biesiadajakub@icloud.com>
2021-03-02 23:22:03 +0100

    feat: add onboarding tips

--- src/ui/components/Elements/Elements.tsx
+++ src/ui/components/Elements/Elements.tsx
@@ -41,6 +41,27 @@ export const Elements = () => {
 
   return (
     <div className={configurators.length > 0 ? undefined : styles.fullLayout}>
+      {/* <Onboarding iconProps={{ iconName: "minus" }}>
+        Select Parts, Nodes and Paints to invert. You can use search field to filter options. By clicking on group heading e.g.: Paints you can toggle all visible items for group.
+      </Onboarding> */}
+
+      {/* TODO */}
+      {/* <Onboarding iconProps={{ iconName: "close" }}>
+        Select Parts, Nodes and Paints to invert.
+      </Onboarding>
+
+      <Onboarding iconProps={{ iconName: "close" }}>
+        You can use search field to filter options. By clicking on group heading e.g.: Paints you can toggle all visible items for group.
+      </Onboarding>
+
+      <Onboarding iconProps={{ iconName: "close" }}>
+        By clicking on group heading e.g.: Paints you can toggle all visible items for group.
+      </Onboarding>
+
+      <Onboarding iconProps={{ iconName: "close" }}>
+        Cancel, Save, Save & Invert
+      </Onboarding> */}
+
       <div className={styles.toolbar}>
         <Input
           value={searchValue}
